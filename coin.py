871df083ce2bed8d9d3bf7fa843ff0f15a2d989e
SirLez <76112117+SirLez@users.noreply.github.com>
2021-01-09 02:58:51 +0300

    Update coin.py

--- coin.py
+++ coin.py
@@ -1,5 +1,4 @@
 import amino
-import getpass
 import os
 os.system("clear")
 print("\033[1;36m Amino :  \033[1;34mSirLez \n \n \033[1;36minsta : \033[1;34m SirLe0x \n \n \n")
@@ -11,13 +10,13 @@ tst=False
 while tst==False:
 	try:
 		email=input("\033[1;31m\n# ur Email : \033[1;0m")
-		password=getpass.getpass("\033[1;31m# ur Password : \033[1;0m")
+		password=input("\033[1;31m# ur Password : \033[1;0m")
 		client.login(email=email,password=password)
 		tst=True
 	except:
 		tst=False
-		print("\n# Verify ur account! \n")
-		exx=input("# continue? y/n : ")
+		print("\n\033[1;33m# Verify ur account! \n")
+		exx=input("# continue?\033[1;32m y/n : \033[1;0m")
 		if exx=='N' or exx=='n' or exx=='no':
 				os._exit(1)
 
@@ -33,18 +32,12 @@ cht=cht.objectId
 
 print("\n\n\033[1;33mThe TransactionId : \033[1;0m"+tId.pop())
 
-tran=input("\n\n\033[1;33mGive Me The TransactionId :\033[1;0m ")
+tran=input("\n\n\033[1;33m# Give Me The TransactionId :\033[1;0m ")
+
 while True:
-	coins=input("\n\n\033[1;33mHow Much Coins ? :\033[1;0m ")
-
-        subclient.send_coins(chatId=cht,coins="1",transactionId=tran)
-
-        subclient.send_coins(chatId=cht,coins=coins,transactionId=tran)
-
-        subclient.send_coins(chatId=cht,coins=coins,transactionId=tran)
-
+	coins=input("\n\n\033[1;33m# How Much Coins ? :\033[1;0m ")
+	subclient.send_coins(chatId=cht,coins="1",transactionId=tran)
 	subclient.send_coins(chatId=cht,coins=coins,transactionId=tran)
-
 	subclient.send_coins(chatId=cht,coins=coins,transactionId=tran)
-
+	subclient.send_coins(chatId=cht,coins=coins,transactionId=tran)
 	print("\n\033[1;36mDone...")
